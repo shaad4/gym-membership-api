71e8115da9889d15f42b1d1e6e84f1e2e87d975c
shaad4 <mohammedshaadn@gmail.com>
2026-04-10 11:50:12 +0530

    fix(gyms): add queryset to viewsets and implement ownership-based filtering with get_queryset and perform_create

--- gyms/views.py
+++ gyms/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 from .models import Gym, Membership
 from .serializers import GymSerializer, MembershipSerializer
 from .permissions import IsOwner
+
 # Create your views here.
 
 class GymViewSet(ModelViewSet):
     queryset = Gym.objects.all()
     serializer_class = GymSerializer
     permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Gym.objects.filter(owner = self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(owner = self.request.user)
@@ -20,3 +25,13 @@ class MembershipViewSet(ModelViewSet):
     serializer_class = MembershipSerializer
     permission_classes = [IsAuthenticated, IsOwner]
 
+    def get_queryset(self):
+        return Membership.objects.filter(gym__owner = self.request.user)
+
+    def perform_create(self, serializer):
+        gym = serializer.validated_data['gym']
+
+        if gym.owner != self.request.user:
+            raise PermissionDenied("You can only add plans to your own gym")
+
+        serializer.save()
